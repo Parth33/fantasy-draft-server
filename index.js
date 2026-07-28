0763d72fca3d0660641b22188d69d58a58b16c1f
Parth Bhatt <parthbhat@gmail.com>
2026-07-28 17:46:07 -0400

    Change server port from 3001 to 8080

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const xml2js = require("xml2js");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
